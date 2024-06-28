2a77c6b6f33f66e0787fe1e41a7a6508cca8e51f
liujiawei <zj.liujiawei@aisino.com>
2024-06-28 16:01:39 +0800

    add

--- smartdns_gfwlist.sh
+++ smartdns_gfwlist.sh
@@ -6,11 +6,11 @@ sed -i 's/server=/nameserver\ /g' smartdns_gfwlist.conf
 sed -i 's/127.0.0.1#5353/gfwlist/g' smartdns_gfwlist.conf
 while IFS= read -r addLine || [[ -n "$addLine" ]]; do
   echo "nameserver /$addLine/gfwlist" >>smartdns_gfwlist.conf
-done <addon_domain.txt
+done <domain/additional_domain.txt
 
 while IFS= read -r deleteLine || [[ -n "$deleteLine" ]]; do
   sed -i "/$deleteLine/d" smartdns_gfwlist.conf
-done <delete_domain.txt
+done <domain/exclude_domain.txt
 
 git add .
 git commit -m 'update'
