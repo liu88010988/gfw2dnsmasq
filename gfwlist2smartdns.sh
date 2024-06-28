d00b0c28793ae6d2f9bca40da33b45c5a50007d6
liujiawei <zj.liujiawei@aisino.com>
2024-06-28 13:46:19 +0800

    add

--- gfwlist2smartdns.sh
+++ gfwlist2smartdns.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
-#
-# Update GFWList
-# rm -rf /etc/smartdns/dnsmasq_gfwlist.conf
+
 sh dnsmasq_gfwlist.sh -o dnsmasq_gfwlist.conf
 cp -f dnsmasq_gfwlist.conf smartdns_gfwlist.conf
 sed -i 's/server=/nameserver\ /g' smartdns_gfwlist.conf
 sed -i 's/127.0.0.1#5353/gfwlist/g' smartdns_gfwlist.conf
-sed -i '/m-team.cc/d' smartdns_gfwlist.conf
-echo 'nameserver /javdb.com/gfwlist' >> smartdns_gfwlist.conf
-echo 'nameserver /ipleak.net/gfwlist' >> smartdns_gfwlist.conf
-echo 'nameserver /docker.com/gfwlist' >> smartdns_gfwlist.conf
+cat addon_domain | while read addLine; do
+  echo "nameserver /$addLine/gfwlist" >>smartdns_gfwlist.conf
+done
+
+cat delete_domain | while read deleteLine; do
+  sed -i "/$deleteLine/d" smartdns_gfwlist.conf
+done
+
 git add .
 git commit -m 'update'
 git push
