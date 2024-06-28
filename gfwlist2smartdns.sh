e2da2f9a525a256f5d0656f9106c546b435f561f
liujiawei <zj.liujiawei@aisino.com>
2024-06-28 13:12:30 +0800

    add

--- gfwlist2smartdns.sh
+++ gfwlist2smartdns.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
 #
 # Update GFWList
-rm -rf /etc/smartdns/dnsmasq_gfwlist.conf
-sh dnsmasq_gfwlist.sh -o /etc/smartdns/dnsmasq_gfwlist.conf
-sed -i 's/server=/nameserver\ /g' /etc/smartdns/dnsmasq_gfwlist.conf
-sed -i 's/127.0.0.1#5353/gfwlist/g' /etc/smartdns/dnsmasq_gfwlist.conf
-sed -i '/m-team.cc/d' /etc/smartdns/dnsmasq_gfwlist.conf
-echo 'nameserver /javdb.com/gfwlist' >> /etc/smartdns/dnsmasq_gfwlist.conf
-echo 'nameserver /ipleak.net/gfwlist' >> /etc/smartdns/dnsmasq_gfwlist.conf
-echo 'nameserver /docker.com/gfwlist' >> /etc/smartdns/dnsmasq_gfwlist.conf
-echo 'restart smartdns'
-systemctl restart smartdns
+# rm -rf /etc/smartdns/dnsmasq_gfwlist.conf
+sh dnsmasq_gfwlist.sh -o dnsmasq_gfwlist.conf
+cp -f dnsmasq_gfwlist.conf smartdns_gfwlist.conf
+sed -i 's/server=/nameserver\ /g' smartdns_gfwlist.conf
+sed -i 's/127.0.0.1#5353/gfwlist/g' smartdns_gfwlist.conf
+sed -i '/m-team.cc/d' smartdns_gfwlist.conf
+echo 'nameserver /javdb.com/gfwlist' >> smartdns_gfwlist.conf
+echo 'nameserver /ipleak.net/gfwlist' >> smartdns_gfwlist.conf
+echo 'nameserver /docker.com/gfwlist' >> smartdns_gfwlist.conf
+git add .
+git commit -m 'update'
+git push
+git push -f https://gitee.com/hz-liujiawei/gfw2dnsmasq.git master
+#echo 'restart smartdns'
+#systemctl restart smartdns
