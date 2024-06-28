bf99e1d781911011b4d9ab9d235ccfcbb1c3872c
liujiawei <zj.liujiawei@aisino.com>
2024-06-28 14:39:35 +0800

    移除hub.docker.com

--- gfwlist2smartdns.sh
+++ gfwlist2smartdns.sh
@@ -6,11 +6,11 @@ sed -i 's/server=/nameserver\ /g' smartdns_gfwlist.conf
 sed -i 's/127.0.0.1#5353/gfwlist/g' smartdns_gfwlist.conf
 while IFS= read -r addLine || [[ -n "$addLine" ]]; do
   echo "nameserver /$addLine/gfwlist" >>smartdns_gfwlist.conf
-done < addon_domain
+done <addon_domain
 
 while IFS= read -r deleteLine || [[ -n "$deleteLine" ]]; do
   sed -i "/$deleteLine/d" smartdns_gfwlist.conf
-done < delete_domain
+done <delete_domain
 
 git add .
 git commit -m 'update'
