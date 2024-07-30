cc75048889dfebf2bec6273cdd7692da5fc6da75
liujiawei <liujiawei@163.com>
2024-07-30 20:17:57 +0800

    daily update

--- mosdns.sh
+++ mosdns.sh
@@ -28,15 +28,6 @@ cp -f domain/proxy-list.txt "$work_dir"
 cp -f mosdns/hosts.txt "$work_dir"
 cp -f mosdns/mosdns.yaml "$work_dir"
 
-script_name=$(basename "$0")
-pid=$(pgrep -f "mosdns.yaml" | grep -v "$script_name" | grep -v "grep")
-# 如果找到进程，则杀死它
-if [ -n "$pid" ]; then
-  echo "killing existing mosdns PID: $pid"
-  kill -9 "$pid"
-  echo "Process mosdns $pid has terminated."
-fi
-
 # 启动mosdns
 echo 'restart mosdns'
 systemctl enable mosdns
