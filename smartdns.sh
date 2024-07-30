53baa0f42b33fcaac0d7ee4080873a8dc7eadb97
liujiawei <zj.liujiawei@aisino.com>
2024-07-30 15:58:38 +0800

    add

---
+++ smartdns.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+group=$1
+if [ -z "$group" ]; then
+  group=gfwlist
+fi
+echo "use smartdns group $group"
+
+cp -f proxy-list.txt smartdns_proxy.conf
+sed -i 's/^/nameserver\ \//g' smartdns_proxy.conf
+sed -i "s/$/\/$group/g" smartdns_proxy.conf
+
+# 更新并重启smartdns
+cp -f smartdns_proxy.conf /etc/smartdns/smartdns_proxy.conf
+echo 'restart smartdns'
+systemctl restart smartdns
