446b98700dc2285e4413357fecf6253feff01864
liujiawei <zj.liujiawei@aisino.com>
2024-07-30 14:54:52 +0800

    sh

---
+++ start-mosdns.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+work_dir="/etc/mosdns"
+
+if [ ! -d "$work_dir" ]; then
+  mkdir -p "$work_dir"
+fi
+
+# 配置文件拷贝
+cp -f mosdns/apple-cn.txt "$work_dir"
+cp -f mosdns/china-ip-list.txt "$work_dir"
+cp -f mosdns/direct-list.txt "$work_dir"
+cp -f mosdns/hosts "$work_dir"
+cp -f mosdns/proxy-list.txt "$work_dir"
+cp -f mosdns/mosdns.yaml "$work_dir"
+
+script_name=$(basename "$0")
+pid=$(pgrep -f "mosdns" | grep -v "$script_name")
+# 如果找到进程，则杀死它
+if [ -n "$pid" ]; then
+  echo "killing existing mosdns PID: $pid"
+  kill -9 "$pid"
+  echo "Process mosdns $pid has terminated."
+fi
+
+# 启动mosdns
+cd "$work_dir"
+mosdns start -c mosdns.yaml -d "$work_dir"
