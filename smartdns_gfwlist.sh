5d9d920507de6b3c01690e575818846b67b533b1
liujiawei <zj.liujiawei@aisino.com>
2024-07-30 17:11:21 +0800

    rename

--- smartdns_gfwlist.sh
+++ smartdns_gfwlist.sh
@@ -8,25 +8,25 @@ echo "use smartdns group $group"
 
 # 生成并处理dnsmasq_gfwlist.conf
 sh dnsmasq_gfwlist.sh -o dnsmasq_gfwlist.conf
-cp -f dnsmasq_gfwlist.conf smartdns_gfwlist.conf
-sed -i 's/server=/nameserver\ /g' smartdns_gfwlist.conf
-sed -i "s/127.0.0.1#5353/$group/g" smartdns_gfwlist.conf
+cp -f dnsmasq_gfwlist.conf smartdns/smartdns_gfwlist.conf
+sed -i 's/server=/nameserver\ /g' smartdns/smartdns_gfwlist.conf
+sed -i "s/127.0.0.1#5353/$group/g" smartdns/smartdns_gfwlist.conf
 
 # 添加额外的域名
 if [[ -f domain/additional_domain.txt ]]; then
   while IFS= read -r additional || [[ -n "$additional" ]]; do
-    echo "nameserver /$additional/$group" >>smartdns_gfwlist.conf
+    echo "nameserver /$additional/$group" >>smartdns/smartdns_gfwlist.conf
   done <domain/additional_domain.txt
 fi
 
 # 删除排除的域名
 if [[ -f domain/exclude_domain.txt ]]; then
   while IFS= read -r exclude || [[ -n "$exclude" ]]; do
-    sed -i "/$exclude/d" smartdns_gfwlist.conf
+    sed -i "/$exclude/d" smartdns/smartdns_gfwlist.conf
   done <domain/exclude_domain.txt
 fi
 
 # 更新并重启smartdns
-cp -f smartdns_gfwlist.conf /etc/smartdns/smartdns_gfwlist.conf
+cp -f smartdns/smartdns_gfwlist.conf /etc/smartdns/smartdns_gfwlist.conf
 echo 'restart smartdns'
 systemctl restart smartdns
--- smartdns_proxy.sh
+++ smartdns_proxy.sh
@@ -6,13 +6,13 @@ if [ -z "$group" ]; then
 fi
 echo "use smartdns group $group"
 
-cp -f domain/proxy-list.txt smartdns_proxy.conf
-sed -i 's/^/nameserver\ \//g' smartdns_proxy.conf
-sed -i "s/$/\/$group/g" smartdns_proxy.conf
-sed -i 's/full://g' smartdns_proxy.conf
-sed -i "/regexp:/d" smartdns_proxy.conf
+cp -f domain/proxy-list.txt smartdns/smartdns_proxy.conf
+sed -i 's/^/nameserver\ \//g' smartdns/smartdns_proxy.conf
+sed -i "s/$/\/$group/g" smartdns/smartdns_proxy.conf
+sed -i 's/full://g' smartdns/smartdns_proxy.conf
+sed -i "/regexp:/d" smartdns/smartdns_proxy.conf
 
 # 更新并重启smartdns
-cp -f smartdns_proxy.conf /etc/smartdns/smartdns_proxy.conf
+cp -f smartdns/smartdns_proxy.conf /etc/smartdns/smartdns_proxy.conf
 echo 'restart smartdns'
 systemctl restart smartdns
