2ae630bbf0052638c7bef1cce1997d97119e9c57
liujiawei <zj.liujiawei@aisino.com>
2024-08-02 13:36:10 +0800

    add

--- mosdns.sh
+++ mosdns.sh
@@ -12,18 +12,18 @@ mkdir -p "$ip_dir" "$domain_dir" "$hosts_dir"
 # 更新并复制配置文件
 ./update-data.sh
 
-cp -f ip/geoip_cn.txt "$ip_dir"
-cp -f ip/geoip_private.txt "$ip_dir"
-cp -f domain/geosite_apple-cn.txt "$domain_dir"
-cp -f domain/geosite_google-cn.txt "$domain_dir"
-cp -f domain/geosite_cn.txt "$domain_dir"
-cp -f domain/geosite_private.txt "$domain_dir"
-cp -f domain/geosite_geolocation-!cn.txt "$domain_dir"
-cp -f domain/geosite_tld-cn.txt "$domain_dir"
-cp -f domain/geosite_category-games@cn.txt "$domain_dir"
+cp -f "ip/geoip_cn.txt" "$ip_dir"
+cp -f "ip/geoip_private.txt" "$ip_dir"
+cp -f "domain/geosite_apple-cn.txt" "$domain_dir"
+cp -f "domain/geosite_google-cn.txt" "$domain_dir"
+cp -f "domain/geosite_cn.txt" "$domain_dir"
+cp -f "domain/geosite_private.txt" "$domain_dir"
+cp -f "domain/geosite_geolocation-!cn.txt" "$domain_dir"
+cp -f "domain/geosite_tld-cn.txt" "$domain_dir"
+cp -f "domain/geosite_category-games@cn.txt" "$domain_dir"
 # 复制其他配置文件
-cp -f hosts/hosts.txt "$hosts_dir"
-cp -f mosdns/mosdns.yaml "$work_dir"
+cp -f "hosts/hosts.txt" "$hosts_dir"
+cp -f "mosdns/mosdns.yaml" "$work_dir"
 
 # 检查并启用 mosdns 服务
 service_file="/etc/systemd/system/mosdns.service"
