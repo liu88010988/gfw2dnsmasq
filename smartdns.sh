9fb2d09941e4e8b92a4c7f151183fa0a76dd0b77
liujiawei <zj.liujiawei@aisino.com>
2024-08-06 08:03:09 +0800

    fix code

--- smartdns.sh
+++ smartdns.sh
@@ -8,12 +8,10 @@ mkdir -p "$work_dir"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') 开始更新并重启SmartDNS服务..."
 echo "$(date '+%Y-%m-%d %H:%M:%S') 使用SmartDNS组:$group..."
-
-echo "$(date '+%Y-%m-%d %H:%M:%S') 生成SmartDNS配置..."
 bash update-data.sh "$group"
 
 # 更新 SmartDNS 配置并重启服务
-cp -f smartdns/smartdns_proxy.conf smartdns/smartdns_gfwlist.conf $work_dir
+cp -f smartdns/smartdns_proxy.conf smartdns/smartdns_gfwlist.conf "$work_dir"
 echo "$(date '+%Y-%m-%d %H:%M:%S') 重启SmartDNS服务..."
 systemctl restart smartdns
 echo "$(date '+%Y-%m-%d %H:%M:%S') 更新并重启SmartDNS服务完成..."
